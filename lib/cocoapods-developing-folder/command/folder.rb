dabd884854a348f0bad439bc6f1a4df7edcff311
leavez <gaojiji@gmail.com>
2018-01-04 18:04:32 +0800

    rename

--- lib/cocoapods-developing-folder/command/folder.rb
+++ lib/cocoapods-developing-folder/command/folder.rb
@@ -1,7 +1,7 @@
 module Pod
     class Podfile
         def use_folders
-            require 'cocoapods-developing-folder/command/patch.rb'
+            require 'cocoapods-developing-folder/command/preserve_folder.rb'
         end
     end
 end
