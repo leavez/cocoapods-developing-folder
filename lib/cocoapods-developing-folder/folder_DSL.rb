34901cfdae9a87eed0c22d79afa8e2d433b4f341
GaoJi <gaoji@mobike.com>
2018-05-08 20:05:34 +0800

    update folder function

--- lib/cocoapods-developing-folder/folder_DSL.rb
+++ lib/cocoapods-developing-folder/folder_DSL.rb
@@ -4,22 +4,24 @@ module Pod
     class Podfile
         module DSL
 
-            def folder(path)
+            def folder(path, *requirements)
                 basePath = Pathname.new path
-                def import_pod(path)
+                def import_pod(path, *requirements)
                     podspec = path.children.find do |p|
                         !p.directory? and p.extname == ".podspec"
                     end
                     if podspec != nil 
-                        pod podspec.basename(".podspec").to_s, :path => path.to_path
+                        options = (requirements.last || {}).clone 
+                        options[:path] = path.to_path
+                        pod(podspec.basename(".podspec").to_s, options)
                     end
                     path.children.each do |p|
                         if p.directory?
-                            import_pod(p)
+                            import_pod(p, *requirements)
                         end
                     end
                 end
-                import_pod basePath
+                import_pod basePath, *requirements
             end
 
         end
